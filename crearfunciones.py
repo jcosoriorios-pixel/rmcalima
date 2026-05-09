849b0b35f076015d2064d7633ed0772af9e1ab18
Rodriguez Carlos <Rodriguez15082002@gmail.com>
2026-05-09 17:01:46 -0500

    Borro archivos extra (boloncho)

--- crearfunciones.py
+++
@@ -1,9 +0,0 @@
-def saludo():
-    print("Hola, bienvenido a utedeC2")
-
-saludo()
-
-def saludo_nombre(nombre):
-    print(f"Hola {nombre}, bienvenido a utedeC2")
-
-saludo_nombre("boloncho") 
--- ejerciciosdeigo.py/ejercicio11.py
+++
@@ -1,7 +0,0 @@
-# Ejercicio 11: Contar hacia atrás desde un número positivo
-
-numero = int(input("Ingresa un número positivo: "))
-
-while numero >= 0:
-    print(numero)
-    numero -= 1
